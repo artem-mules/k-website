ddd52d592bd9171ea7c0400c20e2165029f3f8b6
Artem Kopytok <13ar.bass@gmail.com>
2021-09-08 18:13:12 +0300

    deleted (*)

--- detail-page.js
+++ detail-page.js
@@ -3,30 +3,36 @@ let popUpDatavalue = popUpDataEl.textContent;
 let popupWrapper = document.querySelector('.popup-block__2');
 
 let parsed = JSON.parse(popUpDatavalue);
-console.log(parsed);
 
 
 parsed.forEach(parsedObj => {
     for (const [key, value] of Object.entries(parsedObj)) {
-        // console.log(`${key}: ${value}`);
         if ([key] == 'name-of-section') {
             let clonableSectionHeader = document.querySelector('.pop-up-row--section-name').cloneNode(true);
             clonableSectionHeader.querySelector('.pop-up-row__text').textContent = `${value}`;
             popupWrapper.append(clonableSectionHeader);
-            console.log('⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️name of section is:');
-            console.log(`${value}`);
-            console.log('and value of columns is: ');
         } else {
+            let cleanArray = new Array();
             let valueSplitted = `${value}`;
             valueSplitted = valueSplitted.split('@');
-            console.log(valueSplitted);
-            // console.log(`${value}`);
+            valueSplitted.forEach(splittedItem => {
+                if (splittedItem == '(*)' || splittedItem == ' (*)' || splittedItem == '(*)' || splittedItem == ' (*) ' || splittedItem == '  (*)') {
+
+                    splittedItem = ''
+
+                    cleanArray.push(splittedItem);
+                } else {
+                    cleanArray.push(splittedItem);
+                }
+            });
+
+
 
             let clonableSectionDataRow = document.querySelector('.pop-up-row--data').cloneNode(true);
             let rowPlaceHolder1 = clonableSectionDataRow.querySelector('.pop-up-row__text__1');
             let rowPlaceHolder2 = clonableSectionDataRow.querySelector('.pop-up-row__text__2');
-            rowPlaceHolder1.textContent = valueSplitted[0];
-            rowPlaceHolder2.textContent = valueSplitted[1];
+            rowPlaceHolder1.textContent = cleanArray[0];
+            rowPlaceHolder2.textContent = cleanArray[1];
             popupWrapper.append(clonableSectionDataRow);
         }
     }
