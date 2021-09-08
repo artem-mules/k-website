c3f4ef35b09a8bdbd20f4d471b01a0a109c07eff
Artem Kopytok <13ar.bass@gmail.com>
2021-09-08 17:18:03 +0300

    before: delete cpnsole and empty space

--- detail-page.js
+++ detail-page.js
@@ -1,5 +1,6 @@
 let popUpDataEl = document.querySelector('.popup-data');
 let popUpDatavalue = popUpDataEl.textContent;
+let popupWrapper = document.querySelector('.popup-block__2');
 
 let parsed = JSON.parse(popUpDatavalue);
 console.log(parsed);
@@ -9,11 +10,24 @@ parsed.forEach(parsedObj => {
     for (const [key, value] of Object.entries(parsedObj)) {
         // console.log(`${key}: ${value}`);
         if ([key] == 'name-of-section') {
+            let clonableSectionHeader = document.querySelector('.pop-up-row--section-name').cloneNode(true);
+            clonableSectionHeader.querySelector('.pop-up-row__text').textContent = `${value}`;
+            popupWrapper.append(clonableSectionHeader);
             console.log('⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️name of section is:');
             console.log(`${value}`);
             console.log('and value of columns is: ');
         } else {
-            console.log(`${value}`);
+            let valueSplitted = `${value}`;
+            valueSplitted = valueSplitted.split('@');
+            console.log(valueSplitted);
+            // console.log(`${value}`);
+
+            let clonableSectionDataRow = document.querySelector('.pop-up-row--data').cloneNode(true);
+            let rowPlaceHolder1 = clonableSectionDataRow.querySelector('.pop-up-row__text__1');
+            let rowPlaceHolder2 = clonableSectionDataRow.querySelector('.pop-up-row__text__2');
+            rowPlaceHolder1.textContent = valueSplitted[0];
+            rowPlaceHolder2.textContent = valueSplitted[1];
+            popupWrapper.append(clonableSectionDataRow);
         }
     }
 });
