f8fb0d02a1dc55e6edf297c62e3f503a5b7acbb7
Artem Kopytok <13ar.bass@gmail.com>
2021-10-24 20:52:56 +0300

    created: date replacer

---
+++ date--shor.js
@@ -0,0 +1,39 @@
+    let months = document.querySelectorAll('.mounth');
+            months.forEach(element => {
+                if (element.textContent == 'Jan') {
+        element.textContent = 'Jan'
+    }
+    if (element.textContent == 'Feb') {
+        element.textContent = 'Feb'
+    }
+    if (element.textContent == 'Mar') {
+        element.textContent = 'Mär'
+    }
+    if (element.textContent == 'Apr') {
+        element.textContent = 'Apr'
+    }
+    if (element.textContent == 'May') {
+        element.textContent = 'Mai'
+    }
+    if (element.textContent == 'Jun') {
+        element.textContent = 'Jun'
+    }
+    if (element.textContent == 'Jul') {
+        element.textContent = 'Jul'
+    }
+    if (element.textContent == 'Aug') {
+        element.textContent = 'Aug'
+    }
+    if (element.textContent == 'Sep') {
+        element.textContent = 'Sep'
+    }
+    if (element.textContent == 'Oct') {
+        element.textContent = 'Okt'
+    }
+    if (element.textContent == 'Nov') {
+        element.textContent = 'Nov'
+    }
+    if (element.textContent == 'Dec') {
+        element.textContent = 'Dez'
+    }
+});
