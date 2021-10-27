3e0cf3d33ed405ed4ab3349edda195fed2cb863e
Artem Kopytok <13ar.bass@gmail.com>
2021-10-27 06:55:17 +0300

    updated: new tab

--- page-transition.js
+++ page-transition.js
@@ -5,13 +5,15 @@ allHrefOnThePage.forEach(link => {
     link.addEventListener('click', function (event) {
         let currentHrefValue = link.getAttribute('href');
 
-        if (link.getAttribute('href') != null && currentHrefValue.charAt(0) != '#') {
+        if (link.getAttribute('href') != null && currentHrefValue.charAt(0) != '#' && link.getAttribute('target') != '_blank') {
             event.preventDefault();
             function goToLink() {
                 window.location = currentHrefValue;
             }
             setTimeout(goToLink, 500);
             bodyOfThisPage.classList.add('body-transition');
+        } else {
+            window.open(currentHrefValue, '_blank').focus();
         }
     });
 });
