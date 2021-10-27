d143618524eaa5403fa8e72a427cefbf4ee20463
Artem Kopytok <13ar.bass@gmail.com>
2021-10-27 07:34:41 +0300

    updated: month and scroll to parent section

--- news-arc.js
+++ news-arc.js
@@ -1,3 +1,63 @@
+function pagOnClickScroll() {
+    let pagWrapper = document.querySelector('#wf-fslib-pagination');
+    let hiddenPagTrigger = document.querySelector('.hidden-link');
+    pagWrapper.addEventListener('click', function () {
+        setTimeout(hiddenPagTrigger.click(), 300);
+    });
+}
+
+
+
+function translateDateToGerman() {
+    let dateLabels = document.querySelectorAll('.body-3--date');
+
+    dateLabels.forEach(label => {
+        splittedLabel = label.textContent.split(' ')
+        currentNonth = splittedLabel[0];
+
+        if (currentNonth == 'Jan') {
+            currentNonth = 'Jan'
+        }
+
+        if (currentNonth == 'Feb') {
+            currentNonth = 'Feb'
+        }
+        if (currentNonth == 'Mar') {
+            currentNonth = 'Mär'
+        }
+        if (currentNonth == 'Apr') {
+            currentNonth = 'Apr'
+        }
+        if (currentNonth == 'May') {
+            currentNonth = 'Mai'
+        }
+        if (currentNonth == 'Jun') {
+            currentNonth = 'Jun'
+        }
+        if (currentNonth == 'Jul') {
+            currentNonth = 'Jul'
+        }
+        if (currentNonth == 'Aug') {
+            currentNonth = 'Aug'
+        }
+        if (currentNonth == 'Sep') {
+            currentNonth = 'Sep'
+        }
+        if (currentNonth == 'Oct') {
+            currentNonth = 'Okt'
+        }
+        if (currentNonth == 'Nov') {
+            currentNonth = 'Nov'
+        }
+        if (currentNonth == 'Dec') {
+            currentNonth = 'Dez'
+        }
+
+
+        label.textContent = (currentNonth + ' ' + splittedLabel[1] + ' ' + splittedLabel[2]);
+    });
+
+}
 
 (function () {
     fsComponent2 = new FsLibrary('.cl__grid__media-list-wrapper--press')
@@ -33,6 +93,8 @@ function startFinsweetFilter() {
     }
 
     setTimeout(apperMedia, 500);
+    setTimeout(translateDateToGerman, 400);
+    setTimeout(pagOnClickScroll, 600);
 }
 
 function listenToSwitcher() {
