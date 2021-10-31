6e2e9e7d112bdba431e1419352ac137c49dcb427
Artem Kopytok <13ar.bass@gmail.com>
2021-11-01 00:03:49 +0300

    updated: min sub

--- detail-page.js
+++ detail-page.js
@@ -47,3 +47,26 @@ readySectionName.forEach(readyName => {
         currentReadyName.classList.add('border-bottom--none');
     }
 });
+
+
+
+let allP = document.querySelectorAll('p');
+allP.forEach(p => {
+    let pContent = p.textContent;
+    let splittedLine = pContent.split('^');
+    if (splittedLine.length > 1) {
+        p.textContent = '';
+        splittedLine.forEach(el => {
+            if (el == 'max' || el == 'min') {
+                if (el == 'max') {
+                    p.insertAdjacentHTML('beforeend', '<sub>max</sub>');
+                }
+                if (el == 'min') {
+                    p.insertAdjacentHTML('beforeend', '<sub>min</sub>');
+                }
+            } else {
+                p.insertAdjacentHTML('beforeend', el);
+            }
+        });
+    }
+});
--- min-max.js
+++ min-max.js
@@ -1,7 +1,20 @@
-let testString = 'ArtemSolary is*min*15000USD'
-let splittedValue = testString.split('*');
-splittedValue.forEach(text => {
-    if (text == 'min' || text == 'max' ) {
-        textWithSpan = ('<span class="min-max">' + text + '</span>' )
+let allP = document.querySelectorAll('p');
+allP.forEach(p => {
+    let pContent = p.textContent;
+    let splittedLine = pContent.split('^');
+    if (splittedLine.length > 1) {
+        p.textContent = '';
+        splittedLine.forEach(el => {
+            if (el == 'max' || el == 'min') {
+                if (el == 'max') {
+                    p.insertAdjacentHTML('beforeend', '<sub>max</sub>');
+                }
+                if (el == 'min') {
+                    p.insertAdjacentHTML('beforeend', '<sub>min</sub>');
+                }
+            } else {
+                p.insertAdjacentHTML('beforeend', el);
+            }
+        });
     }
 });
