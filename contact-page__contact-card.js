51aec8dc28204d56312dd4f6efb92187b273377d
Artem Kopytok <13ar.bass@gmail.com>
2021-12-01 11:45:12 +0300

    before: find bug

--- contact-page__contact-card.js
+++ contact-page__contact-card.js
@@ -6,18 +6,14 @@ let inputStatesList = document.querySelector('#satetes');
 
 inputCountryList.addEventListener('change', function() {
     if (inputCountryList.value != 'Deutschland') {
-        //делаем перебор всех карточек, и заглядываем в аттрибуты
         allCards.forEach(card => {
             let currentCardData = card.querySelector('.data-el__contact-card-data');
             let currentCardCounryList = currentCardData.getAttribute('data-countries-list');
             let currentCountryArray = currentCardCounryList.split(',');
             let currentCardTriggerOpen = card.querySelector('.cl-i__contact-card__triggers_o');
             let currentCardTriggerClose = card.querySelector('.cl-i__contact-card__triggers_c');
-            // console.log(currentCountryArray);
-            // console.log(inputCountryList.value);
             if (currentCountryArray.includes(inputCountryList.value)) {
                 currentCardTriggerOpen.click();
-                console.log(currentCardCounryList);
             } else {
                 currentCardTriggerClose.click();
             }
@@ -25,6 +21,31 @@ inputCountryList.addEventListener('change', function() {
     } else {
         allTriggersClose.forEach(trigger => {
             trigger.click();
+        });
+    }
+});
+
+inputStatesList.addEventListener('change', function () {
+    if (inputCountryList.value == 'Deutschland') {
+        allCards.forEach(card => {
+            let currentCardData = card.querySelector('.data-el__contact-card-data');
+            let currentCardStatesList = currentCardData.getAttribute('data-states-list');
+            let currentStatesArray = currentCardStatesList.split(',');
+            let currentCardTriggerOpen = card.querySelector('.cl-i__contact-card__triggers_o');
+            let currentCardTriggerClose = card.querySelector('.cl-i__contact-card__triggers_c');
+            if (inputStatesList.value != '' && currentStatesArray.includes(inputStatesList.value) && inputCountryList.value == 'Deutschland') {
+                currentCardTriggerOpen.click();
+                console.log('открываем карточку');
+            } else {
+                currentCardTriggerClose.click();
+                console.log('закрываем карточку');
+            }
+            if (inputStatesList.value == '') {
+                console.log('закрываем всё-всё');
+                allTriggersClose.forEach(trigger => {
+                    trigger.click();
+                });
+            }
         });
     }
 });
