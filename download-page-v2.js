4828650b68864f64e04fa3c7f535500cc8ef8d42
Artem Kopytok <13ar.bass@gmail.com>
2021-12-09 21:58:44 +0300

    before: weGlot test

--- download-page-v2.js
+++ download-page-v2.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+console.log('hello man!');
+
+// let allDownloadDataDivs = document.querySelectorAll('.block__download-data');
+// let sectionsDataArray = new(Array);
+
+// allDownloadDataDivs.forEach(downloadDataDiv => {
+//     let divAttrValue = downloadDataDiv.getAttribute('name-and-order');
+//     if (sectionsDataArray.includes(divAttrValue) == false) {
+//         sectionsDataArray.push(divAttrValue);
+//     } 
+// });
+
+// console.log(sectionsDataArray);
