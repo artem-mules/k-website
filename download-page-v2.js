6fa0e8cb14d92a7318c6743b829c7d5b674706c5
Artem Kopytok <13ar.bass@gmail.com>
2021-12-09 23:45:04 +0300

    before: rest

--- download-page-v2.js
+++ download-page-v2.js
@@ -1,4 +1,25 @@
-console.log('hello man!');
+//и всё таки надо хранить все текстовые значения в начале страницы, аттрибуты тоже нужны безусловно
+//но иначе weGlot не переводит контент
+
+let allDataName = document.querySelectorAll('.data-src');
+let listOfSection = new(Array)
+let pastePlace = document.querySelector('.page-wrapper');
+
+allDataName.forEach(dataName => {
+    let sectionName = dataName.getAttribute('data-section-name').split('/')[1];
+
+    if (listOfSection.includes(sectionName) == false) {
+        listOfSection.push(sectionName);
+    } 
+});
+
+listOfSection.forEach(arrayItem => {
+    let clonableSection = document.querySelector('.section--placeholder').cloneNode(true);
+    clonableSection.classList.remove('section--placeholder');
+    let currentSectionName = clonableSection.querySelector('h2');
+    currentSectionName.textContent = arrayItem;
+    pastePlace.append(clonableSection);
+});
 
 // let allDownloadDataDivs = document.querySelectorAll('.block__download-data');
 // let sectionsDataArray = new(Array);
